fe3feb0528d8e4e7f9af17cc806cab33326209e8
Timi Precious <pt.timonegmail.com>
2023-09-11 12:34:53 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,24 @@
 <template>
-    <div class=" overflow-x-hidden">
-        <SectionsHero />
-        <SectionsClientele />
+    <NuxtLayout name="default">
 
-        <SectionsGridPage />
-        <SectionsInfo />
-        <SectionsInvestor />
-        <SectionsMerch />
-        <SectionsTestimonial />
 
-    </div>
+
+        <div class=" overflow-x-hidden">
+            <SectionsHero />
+            <SectionsClientele />
+
+            <SectionsGridPage />
+            <SectionsInfo />
+            <SectionsInvestor />
+            <SectionsMerch />
+            <SectionsTestimonial />
+
+        </div>
+    </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-
+definePageMeta({layout:false})
 </script>
 
 <style scoped></style>
