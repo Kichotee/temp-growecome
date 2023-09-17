033dc227b7c1c1b5d15df4a1e56306ff463ae4ae
Timi Precious <pt.timonegmail.com>
2023-09-17 01:58:44 +0100

    update

---
+++ pages/solutions.vue
@@ -0,0 +1,241 @@
+<template>
+    <div class="  overflow-x-hidden bg-black">
+        <Navbar />
+        <div class="bg-[#f1f5f9] margin   min-h-[39.9rem] rounded-[0_0_0_6.25rem] pt-[11.4rem]">
+            <div class="container mx-auto">
+
+                <div class="relative ">
+
+                    <div class="flex flex-col items-center justify-center">
+                        <h2 class="headings text-text-blue font-inter">
+                            Growecom: Empowering All and For Everyone
+                        </h2>
+                        <div class="pt-0.62rem] font-medium text-center text-text-blue md:text-[1.5rem] font-sec">
+                            Experience AI-driven solutions tailored to enhance your marketing strategies, regardless of your
+                            business size or goals.
+                        </div>
+                        <CommonButton class="mt-12" background="bg-brand-primary" />
+                    </div>
+                </div>
+                <div
+                    class="absolute left-1/2 2xl:top-[28rem] p-2  rounded-lg -translate-x-1/2 bottom-36 md:-bottom-[12rem]">
+                    <img src="/images/features-image.png " class="max-w-[18rem] md:max-w-[41rem]" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="mt-36 md:mt-[17.69rem] margin">
+            <Companies />
+            <div class="text-center md:mt-[6.25rem] my-[3.5rem] md:mb-[6.25rem] mx-auto max-w-[68rem]">
+
+                <h2 class="headings text-text-misc">
+                    Effortlessly create graphics for your entire product catalog
+                </h2>
+                <p class="md:text-[1.125rem] mt-[0.62rem] leading-normal text-white">
+                    Whether you possess 1 or 1000 products, revolutionize your product photos into conversion-optimized ad
+                    creatives within seconds. Sign up to join businesses scaling their ads with Growecom.io for remarkable
+                    results.
+                </p>
+            </div>
+            <div class="flex flex-col gap-[6.25rem] text-white items-center">
+
+                <div class="flex flex-wrap gap-12 justify-center items-center">
+                    <div class="">
+
+                        <img src="/images/cards-grid.png" class="max-w-[20.3rem] max-h-max" alt="">
+                    </div>
+                    <img src="/images/cards.png" class="max-w-[19.7rem] " alt="">
+                    <div class="">
+
+                        <img src="/images/cards-grid-2.png" class="max-w-[20.3rem]" alt="">
+                    </div>
+                </div>
+                <div class="flex flex-col items-center">
+                    <ReusedButton bg="bg-brand-secondary" />
+                </div>
+            </div>
+        </div>
+        <div class="md:mt-[12.5rem] mt-16 margin pb-12 md:pb-[6.25rem]">
+            <div class="gap-[6.25rem] flex-col flex">
+
+
+                <div class="flex flex-col text-center mx-auto text-white max-w-[69rem] gap-5 justify-center items-center">
+
+                    <h2 class="headings text-text-misc max-w-[69rem]">
+                        Empower your marketing with convincing Texts & Headlines!
+                    </h2>
+                    <p class="text-sm md:text-base leading-normal">
+                        Generate compelling text headlines and content that captivate your audience. Let our AI generate
+                        persuasive messages that drive engagement and deliver impactful marketing outcomes.
+                    </p>
+                </div>
+                <div class="flex flex-wrap justify-center gap-[1.88rem]">
+                    <div class="max-h-max">
+
+                        <img src="/images/concept-3.png" class="md:max-w-[18rem]" alt="">
+                    </div>
+                    <div class=" d:min-h-[25rem]  items-end flex">
+
+                        <img src="/images/concept-3.png" class=" md:max-w-[18rem] max-h-[18rem] " alt="">
+                    </div>
+                    <div class="max-h-max">
+
+                        <img src="/images/concept-3.png" class=" md:max-w-[18rem]" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-[#f1f5f9] margin py-24 md:py-[6.25rem]">
+            <div class="flex flex-col container mx-auto text-center items-center gap-12 md:gap-[6.25rem]">
+                <div class="flex flex-col items-center gap-[1.25rem] ]">
+                    <h2 class="headings   text-text-blue ">
+                        Concept insight: unlock creative performance metrics and timing precision
+                    </h2>
+                    <p class="leading-normal text-sm md:text-1.125rem max-w-[69rem]">
+                        With our Concept Insight, gain clear insights to match your best-performing visuals. Stay updated on
+                        refreshing ad concepts and maintain effective, relevant content. Elevate your advertising strategy
+                        with informed decisions and impactful outcomes.
+                    </p>
+
+                </div>
+                <div class="flex gap-[1.5rem] flex-wrap justify-center">
+                    <img src="/images/features-insight.png" class="max- gap-[1.5rem] flex-wraph-[19rem]" alt="">
+                    <img src="/images/insight-1.png" class="max-h-[12rem] md:max-h-[18.75rem]" alt="">
+                    <img src="/images/insight-1.png" class="max-h-[12rem] md:max-h-[18.75rem]" alt="">
+                </div>
+                <div class=" flex flex-col items-center gap-[1.25rem]">
+                    <CommonButton background="bg-brand-primary" />
+                    <p>Try 100% free for 7 days. <span class="text-text-misc">
+
+                            Cancel Anytime
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- <div
+            class="bg-[#f1f5f9] md:py-[6.25rem] py-[2.25rem] margin text-center  items-center flex flex-col gap-[6.25rem]  text-text-blue">
+            <div class="flex container  flex-col max-w-[68rem] mx-auto gap-[1.25rem]">
+
+                <h2 class="headings mx-auto ">
+                    AI-powered customer service: elevate interactions with unmatched efficiency
+                </h2>
+                <p class="font-sans text-sm leading-normal md:text-[1.125rem]">
+                    Effortlessly enhance customer interactions and support with our AI-powered solution. Configure your AI
+                    customer service with product and service FAQs, complete with suggested answers, ensuring seamless and
+                    personalized customer interactions.
+                </p>
+            </div>
+            <div class="flex justify-center">
+                <img src="/images/features-image.png" class="md:max-w-[45.7rem]" alt="">
+
+            </div>
+            <div class="">
+                <ReusedButton bg="bg-brand-primary" />
+            </div>
+        </div> -->
+        <div class="bg-black margin pt-[6.25rem]">
+            <div class="flex flex-wrap  mx-auto pt-[6.25rm] gap-6 md:gap-[2rem]">
+
+
+<div class=" max-w-max border bg-white px-8 pt-[2.38rem] pb-[3.08rem] flex flex-col gap-[1.68rem] shadow-md rounded-[.625rem]"
+    v-for="item in testimonialData" :key="item.author">
+    <div class="basis-1/5">
+        <img :src="item.comapnylogo" alt="w">
+    </div>
+    <div class=" flex flex-col max-w-[21.875rem] text-sm md:text-base basis-3/5">
+        <blockquote>
+            {{ item.text }}
+        </blockquote>
+    </div>
+    <div class="flex  gap-[0.62rem]">
+        <div class="p-2 bg-gray-200 rounded-xl">
+
+            <img :src="item.imgUrl" class="max-w-[3.625rem]" alt="">
+        </div>
+        <div class="">
+            <p class="font-['Baloo_Bhai_2'] md:text-[1.5rem]">
+                {{ item.author }}
+            </p>
+
+        </div>
+    </div>
+</div>
+</div>
+            <ReusedButton text="text-white" class="my-[6.25rem]" bg="bg-brand-secondary" />
+
+        </div>
+        <div class="bg-[#f1f5f9] margin pb-[6rem] rounded-[0_6.25rem_0_0] overflow-hidden ">
+
+            <LazyFaq />
+        </div>
+
+
+        <SectionsFooter />
+    </div>
+</template>
+
+<script setup lang="ts">
+const gridImage = ["/images/post.png",
+    "/images/story.png",
+    "/images/landscape.png",
+    "/images/vertical.png",
+    "/images/pin.png",
+    "/images/leaderboard.png", "/images/large-leaderboard.png", "/images/half-page.png", "/images/skyscraper.png",
+    "/images/medium-banner.png",
+]
+const brands = [
+    "/images/Accenture.png",
+    "/images/accenture.png",
+    "/images/bing.png",
+    "/images/instagram.png",
+    "/images/facebook.png",
+    "/images/google.png",
+    "/images/twitter-1.png",
+    "/images/pinterest.png",
+    "/images/pinterest.png",
+    "/images/spotify.png",
+    "/images/linkedin.png",
+]
+const flags = [
+    "/images/English.png",
+    "/images/German.png",
+    "/images/French.png",
+    "/images/spanish.png",
+    "/images/italian.png",
+    // "/images/google.png",
+    "/images/korean.png",
+    "/images/empty-1.png",
+    "/images/dutch.png",
+    "/images/japanese.png",
+    "/images/russian.png",
+    "/images/danish.png",
+    "/images/empty-2.png",
+]
+const testimonialData = [
+  {
+    comapnylogo:"/images/testimonial-logo-1.png",
+    text: "Growecom has transformed the way we approach advertising. The AI-generated creatives are nothing short of impressive. Our click-through rates have skyrocketed, and our conversion rates have doubled since using Growecom. It's a game-changer for our business!",
+    author: "John Smith",
+    imgUrl: "/images/testimonial-person-1.png",
+    role: "CEO of SmithTech"
+  },
+  {
+    comapnylogo:"/images/testimonial-logo-2.png",
+
+    text: "“As a marketing agency, time is money, and we need creatives that work. Growecom has become our secret weapon for delivering exceptional results to our clients. The AI-powered creatives are not only effective but also save us hours of design work. It's a win-win!”",
+    author: "Saarah Johnson",
+    imgUrl: "/images/testimonial-person-2.png",
+    role: "Marketing Director at AdVantage Agency"
+  },
+  {
+    comapnylogo:"/images/testimonial-logo-3.png",
+
+    text: "I was skeptical about AI-generated creatives, but Growecom has exceeded my expectations. The ad content it produces is on point, and the platform's insights have helped us fine-tune our advertising strategy. It's like having a virtual creative team that never sleeps!",
+    author: "Emily Davis",
+    imgUrl: "/images/testimonial-person-3.png",
+    role: "E-commerce Entrepreneur"
+  },
+]
+</script>
+
+<style scoped></style>
